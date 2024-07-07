a485409557de35d00090b9997d725eb15266a5a5
Guocork <g1024536444@gmail.com>
2024-07-07 19:55:23 +0800

    test1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use lettre::{message::header::ContentType, transport::smtp::authentication::Credentials, Message, SmtpTransport, Transport};
+
 fn main() {
-    println!("Hello, world!");
+    let email = Message::builder()
+        .from("Cork <g1024536444@gmail.com>".parse().unwrap()) // 你的Gmail账户作为发件人
+        .reply_to("Cork <g1024536444@gmail.com>".parse().unwrap()) // 你的Gmail账户作为回复地址
+        .to("Recipient <1669387225@qq.com>".parse().unwrap()) // 目标邮箱作为收件人
+        .subject("Test Email") // 邮件标题
+        .header(ContentType::TEXT_PLAIN) // 邮件内容类型为纯文本
+        .body(String::from("Hello World!")) // 邮件正文
+        .unwrap();
+
+    // let creds = Credentials::new("".to_owned(), "".to_owned()); // 使用你的Gmail账户名和密码
+
+    // // 使用Gmail的SMTP服务器
+    // let mailer = SmtpTransport::relay("smtp.gmail.com")
+    //     .unwrap()
+    //     .credentials(creds)
+    //     .build();
+    let mailer = SmtpTransport::unencrypted_localhost();
+
+    // 发送邮件
+    match mailer.send(&email) {
+        Ok(_) => println!("Email sent successfully!"),
+        Err(e) => panic!("Could not send email: {}", e),
+    }
 }
